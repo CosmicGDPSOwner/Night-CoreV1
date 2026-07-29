6493baf8a816a45bd818a1617b1551d9d90d7de3
CosmicGDPSOwner <aleksandrrubacev222@gmail.com>
2026-07-29 12:40:58 +0500

    Test Geometry Dash level hashes

--- tests/selftest.php
+++ tests/selftest.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use NightCore\Core\TableNames;
+use NightCore\Protocol\LevelHash;
 use NightCore\Protocol\XorCipher;
 use NightCore\Security\PasswordService;
 
@@ -31,6 +32,21 @@ $encoded = base64_encode(XorCipher::apply($plain, '37526'));
 $gdEncoded = str_replace(['/', '+'], ['_', '-'], $encoded);
 if (XorCipher::decodeGjp($gdEncoded) !== $plain) {
     $failures[] = 'legacy GJP codec';
+}
+
+if (LevelHash::solo('abc') !== '61353676b92a74ba73096ad981f4fb923addffc6') {
+    $failures[] = 'level solo hash';
+}
+
+if (LevelHash::multi([
+    ['levelID' => 123, 'stars' => 5, 'coins' => 1],
+    ['levelID' => 45, 'stars' => 10, 'coins' => 0],
+]) !== 'cff48a23e85d676ca0252c40687c25b1069b7dd2') {
+    $failures[] = 'level multi hash';
+}
+
+if (LevelHash::solo2('1,5,0,123,1,0,0,0') !== '0b35d193384c953d1ff985cfe319913261e4d2ee') {
+    $failures[] = 'level solo2 hash';
 }
 
 $tables = new TableNames('demo_');
